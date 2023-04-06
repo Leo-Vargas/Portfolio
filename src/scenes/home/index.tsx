aaa5d209f6b5224feb47d34a4b451915e93d562a
Leonardo <leonardo.vargas29@gmail.com>
2023-04-06 02:25:12 -0300

    homepage layout complete

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -1,5 +1,8 @@
 import { SelectedPage } from "@/shared/types"
+import AnchorLink from "react-anchor-link-smooth-scroll"
 import Flower3YellowOrange from "@/assets/Flower3YellowOrange.svg"
+import Flower2GreenPurple from "@/assets/Flower2GreenPurple.svg"
+import Flower1PinkYellow from "@/assets/Flower1PinkYellow.svg"
 
 type Props = {
   selectedPage: SelectedPage;
@@ -9,23 +12,41 @@ type Props = {
 const Home = ({ selectedPage, setSelectedPage }: Props) => {
   return (
   <section id="home" className="gap-16 py-10 md:h-full">
-    <div className="w-5/6 md:flex mx-auto items-center justify-center">
+    <div className="w-3/4 md:flex mx-auto items-center justify-center">
       
       <div className="z-10 mt-32 md:basis-3/5">
         <div className="flex flex-col items-center md:items-start">
           <p className="font-semibold">Hey there, I'm</p>
           <h1 className="font-semibold sm:text-8xl text-6xl">Leonardo</h1>
           <h1 className="font-semibold sm:text-8xl text-6xl">Vargas</h1>
-          <p>Front End Developer based in Brazil</p>
-          <div className="flex">
-            <button>Hire me</button>
-            <p>Learn more</p>
+          <p className="py-2">
+            Front End Developer based in Brazil.
+          </p>
+          <div className="flex w-64 justify-around items-center">
+            <AnchorLink
+              className="rounded-md bg-primary-green text-secondary-pink px-7 py-2 hover:text-primary-purple"
+              onClick={() => setSelectedPage(SelectedPage.ContactMe)}
+              href={`#${SelectedPage.ContactMe}`}
+            >
+              Hire me
+            </AnchorLink>
+            <AnchorLink
+              className="underline hover:text-secondary-purple"
+              onClick={() => setSelectedPage(SelectedPage.ContactMe)}
+              href={`#${SelectedPage.ContactMe}`}
+            >
+              Learn More
+            </AnchorLink>
           </div>
         </div>
       </div>
       
-      <div className="flex basis-3/5 justify-center md:ml-40 md:mt-16 md:justify-items-end">
-        <img alt="70's inspired orange flower" src={Flower3YellowOrange} className="h-14 w-14"/>
+      <div className="flex basis-3/5 justify-center mt-4 md:ml-3 md:mt-16 md:justify-items-end">
+        <div className="relative md:h-[500px] md:w-[500px] sm:h-[400px] sm:w-[400px] h-[300px] w-[300px]">
+          <img alt="medium 70's inspired pink flower" src={Flower1PinkYellow} className="absolute h-1/2 w-1/2 left-0 top-0 z-10"/>
+          <img alt="big 70's inspired orange flower" src={Flower3YellowOrange} className="absolute h-3/4 w-3/4 right-0 bottom-0"/>
+          <img alt="small 70's inspired purple flower" src={Flower2GreenPurple} className="absolute h-1/4 w-1/4 bottom-0 left-1"/>
+        </div>
       </div>
     </div>
   </section>
